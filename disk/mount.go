5918e99acb7f2dc3bcc2726db09df397f3aa2a88
GZGavinZhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-12-31 20:41:23 -0500

    Debug mount command

--- disk/mount.go
+++ disk/mount.go
@@ -5,9 +5,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//      http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -166,6 +164,7 @@ func (m *MountManager) Mount(sourcepath, destpath, filesystem string, options ..
 		command = append(command, "--make-private")
 	}
 
+	fmt.Println(command)
 	if err := commands.ExecStdoutArgs("mount", command); err != nil {
 		return err
 	}
